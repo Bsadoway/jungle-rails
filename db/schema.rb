5b97cb4a5152ab080c6e310cc545b758d9a7caf9
Blake Sadoway <blakesadoway@gmail.com>
2018-01-20 00:19:36 +0000

    fixed prduct reference in review table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180119235559) do
+ActiveRecord::Schema.define(version: 20180120001542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20180119235559) do
     t.datetime "updated_at",  null: false
   end
 
+  add_index "reviews", ["product_id"], name: "index_reviews_on_product_id", using: :btree
   add_index "reviews", ["user_id"], name: "index_reviews_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
@@ -79,5 +80,6 @@ ActiveRecord::Schema.define(version: 20180119235559) do
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
   add_foreign_key "products", "categories"
+  add_foreign_key "reviews", "products"
   add_foreign_key "reviews", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -132,5 +132,8 @@ cat3.products.create!({
   price: 2_483.75
 })
 
+## COMMENTS
+
+
 
 puts "DONE!"
