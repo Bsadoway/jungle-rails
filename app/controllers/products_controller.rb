369f5ab5fbc2e994eb30d55b4a4b4a02ae544e57
Blake Sadoway <blakesadoway@gmail.com>
2018-01-20 01:33:37 +0000

    set up functionality for getting reviews on the product page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,17 @@
 class ProductsController < ApplicationController
   skip_before_action :http_basic_authenticate
-  
+
   def index
     @products = Product.all.order(created_at: :desc)
   end
 
   def show
     @product = Product.find params[:id]
+    @reviews = Review.where("product_id= ?", params[:id]).reverse_order
+    p @reviews
+    @users = User.includes(:reviews).distinct
+    #@users = User.includes(:reviews).where("user_id= ?", "user_id")
+    p @users
   end
 
 end
